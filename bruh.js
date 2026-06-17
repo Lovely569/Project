062edd710573d6a26ec22afc98afb702dd9f7d4a
Ani <131550720+ani-menaphire@users.noreply.github.com>
2026-06-17 07:50:21 +0400

    Make a functional undo button

--- bruh.js
+++ bruh.js
@@ -1,34 +1,78 @@
-
+const undoBtn = document.querySelector(".undo");
 const canvas = document.querySelector("canvas");
+const imgParent = document.querySelector(".img-parent");
 
 // some hotfixes... ( ≖_≖)
 document.body.style.margin = 0;
-canvas.style.position = 'fixed';
+
 
 // get canvas 2D context and set him correct size
 var ctx = canvas.getContext('2d');
-ctx.canvas.width = window.innerWidth;
-  ctx.canvas.height = window.innerHeight;
+ctx.canvas.width = 500;
+ctx.canvas.height = 500;
+
 // last known position
 let X;
 let Y;
+let isPainting = false;
+let history = [];
+let strokeCount = -1;
+console.log(strokeCount);
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', (e)=>{
+  isPainting = true;
+  strokeCount++;
+  setPosition(e);
+});
+document.addEventListener('mouseup', ()=>{
+  isPainting = false;
+
+  if(strokeCount >= 0) {
+    history.splice(strokeCount);
+  }
+
+  //condition makes sure that random mouseup events outside the canvas dont result in duplicate images
+  if (strokeCount >= history.length) {
+    
+    history.push(document.querySelector("canvas").toDataURL());
+
+  }
+})
 
 
-document.addEventListener('mousemove', draw);
-document.addEventListener('mousedown', setPosition);
-document.addEventListener('mouseenter', setPosition);
+undoBtn.addEventListener("click", ()=> {
+
+  if(strokeCount == 0) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    strokeCount--;
+    return;
+  }
+  
+  if(strokeCount > 0) {
+    strokeCount--;
+    let previousImg = new Image();
+    previousImg.src = history[strokeCount];
+    previousImg.onload = ()=> {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(previousImg, 0, 0);
+    }
+  }
+})
+
+// document.addEventListener('mouseenter', setPosition);
 
 // new position from mouse event
 function setPosition(e) {
   X = e.clientX;
-  Y = e.clientY;
+  Y = e.clientY - ctx.canvas.offsetTop;
 }
 
 
 
 function draw(e) {
   // mouse left button must be pressed
-  if (e.buttons !== 1) return;
+  if (!isPainting) return;
 
   ctx.beginPath(); // begin
 
